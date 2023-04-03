a0c50e20fa8640ac9c32693e1314f295c2139d3a
Lore Gostian <loredana.gostian@cobaltsign.com>
2023-04-04 01:14:13 +0300

    MT-8 linked db

--- my_trip_app/lib/main.dart
+++ my_trip_app/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:my_trip_app/screens/landing_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
