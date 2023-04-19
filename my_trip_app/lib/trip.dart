b8c99ed6964bb0c56ea4c0e83b9ea2d765dad3a8
DenisaFleancu26 <denisafleancu2609@gmail.com>
2023-04-19 14:17:11 +0300

    MT-13 connection with db

---
+++ my_trip_app/lib/trip.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:my_trip_app/auth.dart';
+
+class Trip {
+  Future<void> addPlanTrip(
+      String tripStart,
+      String tripEnd,
+      String name,
+      String destination,
+      String hotel,
+      String address,
+      String contact,
+      String checkIn,
+      String checkOut,
+      String transport,
+      String departure,
+      String retur) async {
+    final userQuerySnapshot = await FirebaseFirestore.instance
+        .collection('Users')
+        .where('email', isEqualTo: Auth().currentUser?.email)
+        .get();
+
+    if (userQuerySnapshot.docs.isNotEmpty) {
+      final userId = userQuerySnapshot.docs.first.id;
+      await FirebaseFirestore.instance
+          .collection('Users')
+          .doc(userId)
+          .collection('trip-plan')
+          .add({
+        'start': tripStart,
+        'end': tripEnd,
+        'name': name,
+        'destination': destination,
+        'hotel': hotel,
+        'address': address,
+        'contact': contact,
+        'check-in': checkIn,
+        'check-out': checkOut,
+        'transport': transport,
+        'departure': departure,
+        'return': retur,
+      });
+    }
+  }
+}
