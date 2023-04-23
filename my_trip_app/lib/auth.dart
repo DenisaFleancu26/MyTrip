297d9eea9880174c128e663df9561aa6e9f68ab5
DenisaFleancu26 <denisafleancu2609@gmail.com>
2023-04-23 10:54:37 +0300

    created db for google account

--- my_trip_app/lib/auth.dart
+++ my_trip_app/lib/auth.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -36,10 +37,28 @@ class Auth {
 
     final credential = GoogleAuthProvider.credential(
         accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+
+    googleAccount();
+
     return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
   Future<void> signOut() async {
     await _firebaseAuth.signOut();
+  }
+
+  Future<void> googleAccount() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final userQuerySnapshot = await FirebaseFirestore.instance
+        .collection('Users')
+        .where('email', isEqualTo: googleUser!.email)
+        .get();
+
+    if (userQuerySnapshot.docs.isEmpty) {
+      await FirebaseFirestore.instance.collection('Users').add({
+        'email': googleUser.email,
+        'name': googleUser.displayName,
+      });
+    }
   }
 }
