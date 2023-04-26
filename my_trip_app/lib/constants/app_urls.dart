e6423a815e6bb0ceb13ede12a23c6008a6b40509
Lore Gostian <loredana.gostian@cobaltsign.com>
2023-04-26 13:40:09 +0300

    MT-15 api tokens

---
+++ my_trip_app/lib/constants/app_urls.dart
@@ -0,0 +1,8 @@
+class AppUrls {
+  static const String apiKey =
+      "nAko7DVLoOAq6mb8EzhlortBoVVVssojo8Gap087KaL4tIRBveY23aVt";
+
+  static String getImagesForCountry(String country) {
+    return 'https://api.pexels.com/v1/search?query=$country&per_page=1';
+  }
+}
