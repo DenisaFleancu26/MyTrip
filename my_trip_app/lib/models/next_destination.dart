84200db0bd491dbc9cc2f9322e2f538793258570
Lore Gostian <loredana.gostian@cobaltsign.com>
2023-04-26 13:40:44 +0300

    MT-15 json fetching data

---
+++ my_trip_app/lib/models/next_destination.dart
@@ -0,0 +1,33 @@
+class ImageDestination {
+  final String url;
+
+  ImageDestination({required this.url});
+
+  factory ImageDestination.fromJson(Map<String, dynamic> json) {
+    return ImageDestination(
+        url: json["photos"][0]["src"]["original"] as String);
+  }
+}
+
+class NextDestination {
+  final String destinationName;
+  final String countryName;
+  final String continentName;
+  final List<String> images;
+
+  NextDestination({
+    required this.destinationName,
+    required this.countryName,
+    required this.continentName,
+    required this.images,
+  });
+
+  factory NextDestination.fromJson(Map<String, dynamic> json) {
+    return NextDestination(
+      destinationName: "",
+      countryName: "",
+      continentName: "",
+      images: [],
+    );
+  }
+}
