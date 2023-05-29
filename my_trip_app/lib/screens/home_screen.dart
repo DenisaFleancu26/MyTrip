c2eece052f97a00fed39840c5f3b670d0929ca25
Lore Gostian <loredana.gostian@cobaltsign.com>
2023-05-29 23:24:18 +0300

    MT-29 test case when user doesn't have any plan registered

--- my_trip_app/lib/screens/home_screen.dart
+++ my_trip_app/lib/screens/home_screen.dart
@@ -1,10 +1,11 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:my_trip_app/screens/history.dart';
 import 'package:my_trip_app/screens/trip_plan_screen.dart';
 import 'package:my_trip_app/services/auth.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:my_trip_app/models/next_destination.dart';
 import 'package:my_trip_app/screens/new_trip_plan.dart';
 import 'package:my_trip_app/screens/profile_screen.dart';
@@ -22,7 +23,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final User? user = Auth().currentUser;
   int bottomTabIndex = 0;
 
   late List<Widget> futureDestinations = [];
@@ -32,6 +32,29 @@ class _HomeScreenState extends State<HomeScreen> {
   late NextDestination nextDestination;
   late PastDestination pastDestination;
   late Plan nextPlan;
+
+  final List<Widget> mainTexts = [
+    const Text(
+      "Store your documents",
+      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    ),
+    const Text(
+      "Book your flights",
+      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    ),
+    const Text(
+      "Store your memories",
+      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    ),
+  ];
+
+  int currentIndex = 0;
+
+  final List<Widget> items = [
+    Image.asset('assets/images/passport.png'),
+    Image.asset('assets/images/travel-itinerary.png'),
+    Image.asset('assets/images/image.png'),
+  ];
 
   @override
   void initState() {
@@ -230,7 +253,10 @@ class _HomeScreenState extends State<HomeScreen> {
             child: FutureBuilder(
                 future: _fetchAllData(),
                 builder: (context, snapshot) {
-                  if (snapshot.hasData) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                        child: CircularProgressIndicator(color: Colors.cyan));
+                  } else if (snapshot.hasData) {
                     return Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.start,
@@ -240,34 +266,25 @@ class _HomeScreenState extends State<HomeScreen> {
                           child: Text(
                             "Next Destination",
                             style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                shadows: <Shadow>[
-                                  Shadow(
-                                      offset: Offset(0.5, 0.5),
-                                      blurRadius: 4.0,
-                                      color: Color.fromARGB(159, 66, 66, 66)),
-                                ]),
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                         const SizedBox(height: 15),
                         CustomNextDestination(
                             nextDestination: nextDestination, plan: nextPlan),
                         const Padding(
-                          padding: EdgeInsets.only(left: 25, top: 10),
+                          padding:
+                              EdgeInsets.only(left: 25, top: 20, bottom: 7),
                           child: Text(
                             "Future Destinations",
                             style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                shadows: <Shadow>[
-                                  Shadow(
-                                      offset: Offset(0.5, 0.5),
-                                      blurRadius: 4.0,
-                                      color: Color.fromARGB(159, 66, 66, 66)),
-                                ]),
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                         SizedBox(
@@ -284,15 +301,10 @@ class _HomeScreenState extends State<HomeScreen> {
                             child: Text(
                               "History",
                               style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  shadows: <Shadow>[
-                                    Shadow(
-                                        offset: Offset(0.5, 0.5),
-                                        blurRadius: 4.0,
-                                        color: Color.fromARGB(159, 66, 66, 66)),
-                                  ]),
+                                color: Colors.black,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
                           ),
                         if (pastDestinations.isNotEmpty)
@@ -307,7 +319,77 @@ class _HomeScreenState extends State<HomeScreen> {
                       ],
                     );
                   } else {
-                    return Container();
+                    return Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const SizedBox(height: 20),
+                          const Text("You don't have any plans yet?",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 26,
+                                  fontWeight: FontWeight.bold)),
+                          const SizedBox(height: 5),
+                          const Text(
+                            "What are you waiting for?",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          const SizedBox(height: 50),
+                          CarouselSlider(
+                            options: CarouselOptions(
+                              autoPlay: false,
+                              enlargeCenterPage: true,
+                              onPageChanged: (index, reason) {
+                                setState(() {
+                                  currentIndex = index;
+                                });
+                              },
+                            ),
+                            items: items,
+                          ),
+                          const SizedBox(height: 30),
+                          DotsIndicator(
+                            dotsCount: items.length,
+                            position: currentIndex.toDouble(),
+                            decorator: const DotsDecorator(
+                                color: Color.fromARGB(70, 0, 0, 0),
+                                activeColor: Color.fromARGB(255, 0, 206, 203),
+                                spacing: EdgeInsets.all(4)),
+                          ),
+                          const SizedBox(height: 30),
+                          IndexedStack(
+                            index: currentIndex,
+                            alignment: AlignmentDirectional.center,
+                            children: mainTexts,
+                          ),
+                          const SizedBox(height: 30),
+                          const Text(
+                            "Upload your files and keep them save",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.w400),
+                          ),
+                          const SizedBox(height: 50),
+                          Center(
+                            child: IconButton(
+                              onPressed: () => Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const NewTripPlanScreen())),
+                              icon: const Icon(Icons.add_circle),
+                              color: Colors.cyan,
+                              iconSize: 75,
+                            ),
+                          )
+                        ],
+                      ),
+                    );
                   }
                 })));
   }
